96052f03ccfcd476a76e2235b7258d7e9707e62a
youngjun <juni2004c@gmail.com>
2022-04-04 01:36:27 +0900

    menulist animation 추가

--- src/component/menulist/menulist.jsx
+++ src/component/menulist/menulist.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./menulist.module.css";
 
 const Menulist = ({ isClicked }) => {
   return (
-    <div className={`${styles.wrapper}`}>
-      <div className={`${styles.bar}`}></div>
+    <div className={styles.wrapper}>
+      <div
+        className={`${styles.bar} ${
+          isClicked ? styles.active : styles.inactive
+        }`}
+      ></div>
       <ul className={styles.list}>
-        <li className={`${styles.item}`}>Home</li>
-        <li className={`${styles.item}`}>Videos</li>
-        <li className={`${styles.item}`}>Photos</li>
+        <li
+          className={`${styles.item} ${
+            isClicked ? styles.firstRowIn : styles.firstRowOut
+          }`}
+        >
+          Home
+        </li>
+        <li
+          className={`${styles.item}  ${
+            isClicked ? styles.secondRowIn : styles.secondRowOut
+          }`}
+        >
+          Videos
+        </li>
+        <li
+          className={`${styles.item}  ${
+            isClicked ? styles.thirdRowIn : styles.thirdRowOut
+          }`}
+        >
+          Photos
+        </li>
       </ul>
     </div>
   );
