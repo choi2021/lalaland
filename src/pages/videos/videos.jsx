7aeaa06a473e07d126b850c27d5b660a27a09867
youngjun <juni2004c@gmail.com>
2022-05-01 03:09:18 +0900

    videos btns UI로직처리 완료

--- src/pages/videos/videos.jsx
+++ src/pages/videos/videos.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../../component/header/header";
 import Sidebar from "../../component/sidebar/sidebar";
 import Video from "../../component/video/video";
 import styles from "./videos.module.css";
 
 const Videos = ({ youtube, weatherService }) => {
+  const defaultRef = useRef();
+  const ostRef = useRef();
+  const makingRef = useRef();
+  const sceneRef = useRef();
   const [topic, setTopic] = useState("lalaland");
   const [videos, setVideos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -13,29 +17,69 @@ const Videos = ({ youtube, weatherService }) => {
       setVideos(items);
       setIsLoading(false);
     });
-  }, []);
+  }, [topic]);
 
-  const handleBtnClick = () => {};
+  const handleBtnClick = (event) => {
+    const q = event.currentTarget.dataset.value;
+    setTopic("lalaland" + " " + q);
+  };
 
   return (
     <div className={styles.videos}>
       <div className={styles.bg}></div>
       <div className={styles.header}>
         <Header weatherService={weatherService} location="videos"></Header>
+        <div className={styles.btns}>
+          <button
+            data-value={""}
+            className={`${styles.btn} ${
+              topic === "lalaland " ? styles.active : null
+            }`}
+            ref={defaultRef}
+            onClick={handleBtnClick}
+          >
+            All
+          </button>
+          <button
+            data-value={"OST"}
+            className={`${styles.btn} ${
+              topic === "lalaland OST" ? styles.active : null
+            }`}
+            ref={ostRef}
+            onClick={handleBtnClick}
+          >
+            OST
+          </button>
+          <button
+            data-value={"Making"}
+            className={`${styles.btn} ${
+              topic === "lalaland Making" ? styles.active : null
+            }`}
+            ref={makingRef}
+            onClick={handleBtnClick}
+          >
+            Making
+          </button>
+          <button
+            data-value={"Scene"}
+            className={`${styles.btn} ${
+              topic === "lalaland Scene" ? styles.active : null
+            }`}
+            ref={sceneRef}
+            onClick={handleBtnClick}
+          >
+            FAMOUS SCENE
+          </button>
+        </div>
       </div>
       <div className={styles.menu}>
         <Sidebar></Sidebar>
       </div>
       <div className={styles.main}>
         {isLoading ? (
-          <span className={styles.loading}> Loading videos... 😅</span>
+          <div className={styles.loading}> Loading videos... 😅</div>
         ) : (
           <>
-            <ul className={styles.btns} onClick={handleBtnClick}>
-              <btn>OST</btn>
-              <btn>OST</btn>
-              <btn>OST</btn>
-            </ul>
             <ul className={styles.list}>
               {videos.map((video) => (
                 <Video
